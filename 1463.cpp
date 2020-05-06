33df8369cd9f09d63552538728b4db457b4a3155
i-msy <simc2644@gmail.com>
2020-05-07 03:11:23 +0900

    DP

--- 1463.cpp
+++ 1463.cpp
@@ -1,25 +1,27 @@
 #include <iostream>
 using namespace std;
-int memo[100000];
+//DP(Dynamic Programming) 
+//10^6까지의 숫자를 테스트 한다 
+int memo[1000001]={0,};
+
 int find(int num){
-	if(num==3) return 1;
-	if(num==2) return 1;
-	if(num==1) return 0;
-	for(int i=1;i<=num;i++){
-		if(memo[i]!=0) return memo[i];
-		if(i%2==0){
-			if(memo[i/2]>memo[i-1]) return memo[i]=find(i-1)+1;
-			else return memo[i]=find(i/2)+1;
-		}
-		if(i%3==0){
-			if(memo[i/3]>memo[i-1]) return memo[i]=find(i-1)+1;
-			else return memo[i]=find(i/3)+1;
-		}
+	//down-top  
+	for(int i=2;i<=num;i++){
+		//1을 뺐을 때의 경우를 먼저 저장 
+		memo[i] = memo[i-1]+1;
+		//2와 3으로 나누어 떨어질 때 비교하여 작은 것 선택 
+		if(i%2==0) 
+			if(memo[i] > memo[i/2]) 
+				memo[i]=memo[i/2]+1;
+		if(i%3==0)
+			if(memo[i]>memo[i/3])
+				 memo[i] = memo[i/3]+1;
 	}
-	
+	return memo[num];
 }
 int main(){
 	int num;
 	cin>>num;
 	cout<<find(num);
+	
 }
