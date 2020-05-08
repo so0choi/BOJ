6d33d95a567cdb23a7ddad83437dc2964ca482ef
i-msy <simc2644@gmail.com>
2020-05-08 23:55:48 +0900

    dp-2

---
+++ 11726.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int memo[100000]={0,};
+int find(int n){
+	memo[1] = 1; memo[2] = 2;
+	if(n==1) return 1;
+	if(n==2) return 2;
+	for(int i=3;i<=n;i++){
+		memo[i] = (memo[i-2]+memo[i-1])%10007;
+	}
+	return memo[n];
+}
+int main(){
+	int n;
+	cin>>n;
+	cout<<find(n);
+}
